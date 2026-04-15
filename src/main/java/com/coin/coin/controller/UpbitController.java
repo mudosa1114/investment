75da47f619060cdbc3a43125060cee15e2e04c36
bmkim <bmkim@antock.com>
2026-04-15 12:55:12 +0900

    feat :  - 일별 거래결과 추가

--- src/main/java/com/coin/coin/controller/UpbitController.java
+++ src/main/java/com/coin/coin/controller/UpbitController.java
@@ -2,6 +2,7 @@ package com.coin.coin.controller;
 
 import com.coin.coin.service.UpbitApi;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UpbitController {
 
     private final UpbitApi upbitApi;
+
+    @GetMapping("/code")
+    public String refreshCode() {
+        upbitApi.refreshCoinList();
+        return "success";
+    }
+
+    @GetMapping("/result")
+    public String checkResult() {
+        upbitApi.calculateDailyPnl();
+        return "success";
+    }
 
 
 }
